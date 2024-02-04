a0a286fa82679878036f78c62b004c497b0ed90e
Thomas <tbe@pm.me>
2024-02-04 22:16:17 +0100

    exercise one done, added responses.md

---
+++ responses.md
@@ -0,0 +1,6 @@
+# Cap 2 Exercises
+## REACT Classes
+### 1.
+Only one div can be rendered at a time, so to add more than one, you need to put them inside an outer div.
+### 2. 
+You find the error in the browser console "`main` is not a DOM element"
--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@
 
 class HelloWorld extends React.Component {
 
-    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-    const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-    const message = helloContinents.join(' ');
-
-    render () {
+    
+    render() {
+        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+        const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+        const message = helloContinents.join(' ');
         return (
+            
             <div title='Outer div'>
                 <h1>{message}</h1>
+                <div>
+                    {/* adding a second div is only possible if a div contains them all */}
+                    <h2>TEST</h2>
+                </div>
             </div>
+    
         );
     }
 }
