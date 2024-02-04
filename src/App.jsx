c85317dc914dca1813ac8dcaddc0fa8fbbd40809
Thomas <tbe@pm.me>
2024-02-04 22:05:39 +0100

    first modification cap3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
 
 
 
-const element = (
-    <div title='Outer div'>
-        <h1>{message}</h1>
-    </div>
-);
+class HelloWorld extends React.Component {
+
+    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+    const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+    const message = helloContinents.join(' ');
+
+    render () {
+        return (
+            <div title='Outer div'>
+                <h1>{message}</h1>
+            </div>
+        );
+    }
+}
+
+
+
+
+const element = <HelloWorld />;
 ReactDOM.render(element, document.querySelector('#content'))
